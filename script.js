ade864e3e6ab064c4e829f7becc11dc8cd1a75ef
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-25 20:16:45 +0200

    Update userFunction

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
-            firstName: 'Kellia',
-            lastName: 'Melissa',
-            email: 'kellia@gmail.com',
-            gender: 'female',
-            picture: 'https://randomuser.me/api/portraits/women/8.jpg',
+            firstName: 'Felix',
+            lastName: 'Rasmussen',
+            country: 'Denmark',
+            city: 'Gl. Rye', 
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/15.jpg',
         }
     },
 
@@ -17,7 +18,8 @@ const app = Vue.createApp({
             
             this.firstName = results[0].name.first
             this.lastName = results[0].name.last
-            this.email = results[0].email
+            this.country = results[0].location.country
+            this.city = results[0].location.city
             this.gender = results[0].gender
             this.picture = results[0].picture.large
         }
